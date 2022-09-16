32aaf8f71f1cf4787581160c51a861957b5b98ba
Abdi-Tezera <112860935+Abdi-Tezera@users.noreply.github.com>
2022-09-16 15:19:47 +0200

    Första ändring

---
+++ Git2/Git2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Ange ditt namn:>");
+string name = Console.ReadLine();
+Console.WriteLine("Grattis du har loggat in");
